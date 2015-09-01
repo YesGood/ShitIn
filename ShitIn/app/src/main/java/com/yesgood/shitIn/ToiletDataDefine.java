b3daf798de2360a5f0158802b6f2b34aa74159b0
Bass Yao <bass@boydisown-lm.tpcity.corp.yahoo.com>
2015-09-01 17:22:31 +0800

    remove build and idea

--- ShitIn/app/src/main/java/com/yesgood/shitIn/ToiletDataDefine.java
+++ ShitIn/app/src/main/java/com/yesgood/shitIn/ToiletDataDefine.java
@@ -9,23 +9,20 @@ import java.util.Arrays;
  */
 public class ToiletDataDefine {
 
-    private static Integer[] type_index = {R.id.type_man,R.id.type_woman,R.id.type_all,R.id.type_withchildren,R.id.type_accessible};
-    private static Integer[] type_image = {R.mipmap.ic_men_on,R.mipmap.ic_woman_on,R.mipmap.ic_both_on,R.mipmap.ic_family_on,R.mipmap.ic_handicap_on};
-    private static Integer[] type_image_off = {R.mipmap.ic_men_off,R.mipmap.ic_woman_off,R.mipmap.ic_both_off,R.mipmap.ic_family_off,R.mipmap.ic_handicap_off};
+    private static Integer[] type_index = {R.id.type_man, R.id.type_woman, R.id.type_all, R.id.type_withchildren, R.id.type_accessible};
+    private static Integer[] type_image = {R.mipmap.ic_men_on, R.mipmap.ic_woman_on, R.mipmap.ic_both_on, R.mipmap.ic_family_on, R.mipmap.ic_handicap_on};
+    private static Integer[] type_image_off = {R.mipmap.ic_men_off, R.mipmap.ic_woman_off, R.mipmap.ic_both_off, R.mipmap.ic_family_off, R.mipmap.ic_handicap_off};
 
-    public static int getTypeIndex( int id )
-    {
+    public static int getTypeIndex(int id) {
         return Arrays.asList(type_index).indexOf(id);
     }
 
-    public static int getTypeImageResID(int typeIndex)
-    {
+    public static int getTypeImageResID(int typeIndex) {
         return getTypeImageResID(typeIndex, true);
     }
 
-    public static int getTypeImageResID(int typeIndex, boolean isOn)
-    {
-        if(isOn)
+    public static int getTypeImageResID(int typeIndex, boolean isOn) {
+        if (isOn)
             return type_image[typeIndex];
         else
             return type_image_off[typeIndex];
@@ -33,6 +30,24 @@ public class ToiletDataDefine {
 
     public static Integer[] getType_index() {
         return type_index;
+    }
+
+    public static String getBabyChangingText(Context context, boolean flag) {
+        return flag == true ? context.getResources().getString(R.string.yes_text) : context.getResources().getString(R.string.no_text);
+    }
+
+    public static String getPaperText(Context context, boolean flag) {
+        return flag == true ? context.getResources().getString(R.string.yes_text) : context.getResources().getString(R.string.no_text);
+    }
+
+    public static String getClosetText(Context context, int value) {
+        String[] closet_array = context.getResources().getStringArray(R.array.closet_numeric_array);
+        return closet_array.length > value ? closet_array[value] : "";
+    }
+
+    public static String getUrinalText(Context context, int value) {
+        String[] urinal_array = context.getResources().getStringArray(R.array.urinal_numeric_array);
+        return urinal_array.length > value ? urinal_array[value] : "";
     }
 }
 
